301fe0501e7da8df9e735daf12bc6891bfb713e5
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2026-04-01 18:35:59 +0530

    feat: steup the environment for desktop app

---
+++ desktop_app/electron.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.loadURL('http://localhost:5173'); // Vite dev server
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
