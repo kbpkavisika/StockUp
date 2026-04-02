6992e0c8abf282bc1f5cebc66629f74806c671f6
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2026-04-02 13:27:49 +0530

    chore: update project files

--- desktop_app/src/main.tsx
+++ desktop_app/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './frontend/App'
+import { ThemeProvider } from './frontend/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
