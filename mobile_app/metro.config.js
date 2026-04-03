7d1c7695abd1fa0c65dc4bc5082e80e85ce8b0aa
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2026-04-03 13:57:45 +0530

    chore: update dependencies

--- mobile_app/metro.config.js
+++ mobile_app/metro.config.js
@@ -1,4 +1,2 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('sql');
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
